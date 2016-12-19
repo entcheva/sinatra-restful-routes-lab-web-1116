60f8375c3bf1760f823da7be1e5ba37d8c205b3d
entcheva <christina.entcheva@gmail.com>
2016-12-19 16:19:49 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,40 @@
 class ApplicationController < Sinatra::Base
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+    # placeholder for rspec
     erb :index
   end
+
+  get '/recipes/new' do
+    # form
+    erb :new
+  end
+
+
+  get '/recipes' do
+    @recipes = Recipe.new(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    # displays a list of recipes
+    # contains links to each recipe's show page
+    erb :index
+  end
+
+
+  post '/recipes/:id' do
+    # displays recipe name, ingredients, cook time
+    # contains link to delete the recipe
+    # deletes via delete request
+  end
+
+
+
+
+
+
+
+
 end
